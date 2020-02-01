d68604945d58b022dd1390251f439d6e5d6bca8d
3460741663 <3460741663@qq.com>
2020-02-01 12:32:26 +0800

    登录页面

---
+++ login1/Login.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import InputBox from '../../pages/login/InputBox'
+import styles from './Login.css'
+import widthStyles from '../../withStyles'
+import { Icon } from 'antd'
+class Login extends Component {
+  render() {
+    const form = {};
+    return (
+      <div className={styles.contain}>
+        <div><img src="./image/logo.jpg" className={styles.logo} alt=""/></div>
+        <div className={styles.InputBox}><InputBox form={form}/></div>
+        <div className={styles.quickLogin}>
+          <Icon className={styles.icon} type="qq" />
+          <Icon className={styles.icon} type="wechat" />
+          <Icon className={styles.icon} type="weibo" /></div>
+      </div>
+    );
+  }
+}
+
+export default widthStyles(Login, styles);
