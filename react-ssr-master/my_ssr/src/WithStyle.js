754442e5bb0ee2411022e1a06f2920f0de9a9603
3460741663 <3460741663@qq.com>
2020-12-31 18:47:58 +0800

    别人完整的SSR

---
+++ react-ssr-master/my_ssr/src/WithStyle.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//函数返回组件
+export default (DecoratedComponent, styles) => {
+  return class NewComponent extends Component {
+    componentWillMount() {
+      if (this.props.staticContext) {
+        console.log(this.props)
+        this.props.staticContext.css.push(styles._getCss());
+      }
+    }
+    render() {
+      return <DecoratedComponent {...this.props} />
+    }
+  };
+}
